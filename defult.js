d2e2aa2429e4d6c59942d5e6814e6acec349a84b
JOY1952 <niamurrahmanjoy123@gmail.com>
2021-01-31 23:54:10 +0600

    update

---
+++ defult.js
@@ -0,0 +1,20 @@
+function add(num1=7, num2=20){
+    return num1 + num2;
+}
+
+const total = add(1);
+console.log(total);
+
+const firstName = "Joy";
+const secondName = "Rahman";
+
+const fullName = firstName + " " + secondName;
+const fullName2 = `${firstName} ${secondName} is a good boy`;
+console.log(fullName2);
+
+const multiLine = ` ami tomake bole debo
+keja aka dergo rat
+ami hate gace
+beran potha`
+
+console.log(multiLine);
---
+++ letConst.js
@@ -0,0 +1,12 @@
+const hubby ="Kanchon";
+console.log(hubby);
+
+const numbers = [23,33];
+numbers[1] = 88;
+numbers.push(34);
+// console.log(numbers);
+
+let pasent = "Rohimchaca";
+pasent = "korimchaca";
+
+console.log(pasent);
